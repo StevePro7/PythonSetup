fe43e0eed340376ff69635db6c12513b699274f9
StevePro7 <steveprozune@hotmail.com>
2020-07-15 13:57:58 +0100

    no message

--- HelloPackages/VScode/03-PackageExample/myproject/src/module.py
+++ HelloPackages/VScode/03-PackageExample/myproject/src/module.py
@@ -1,7 +1,7 @@
 # https://stackoverflow.com/questions/53653083/how-to-correctly-set-pythonpath-for-visual-studio-code
 
 def getAge():
-    return 14
+    return 19
     
     
 print('hello there')
--- HelloPackages/VScode/03-PackageExample/myproject/tests/test_module.py
+++ HelloPackages/VScode/03-PackageExample/myproject/tests/test_module.py
@@ -1,11 +1,12 @@
 import unittest
 # from project.src.module import getAge
+from module import getAge
 
 class TestMyFoo(unittest.TestCase):
     def test_lang(self):
-        #age = getAge()
-        age = 16
-        self.assertEqual(age, 15)
+        age = getAge()
+        #age = 16
+        self.assertEqual(age, 16)
 
 if __name__ == '__main__':
     unittest.main()
