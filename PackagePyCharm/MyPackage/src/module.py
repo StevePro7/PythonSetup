044764ee08cfea5f96be5b56ce2c0bcc6954409e
StevePro7 <steveprozune@hotmail.com>
2020-07-16 15:00:21 +0100

    no message

--- PackagePyCharm/MyPackage/src/module.py
+++ PackagePyCharm/MyPackage/src/module.py
@@ -1,2 +1,4 @@
+"""Docstring1"""
 def add_one(number):
+    """Docstring2"""
     return number + 1
--- PackagePyCharm/MyPackage/tests/test_module.py
+++ PackagePyCharm/MyPackage/tests/test_module.py
@@ -1,9 +1,12 @@
+"""Docstring1"""
 import unittest
 from MyPackage.src.module import add_one
 
 class TestSimple(unittest.TestCase):
 
+    """Docstring3"""
     def test_add_one(self):
+        """Docstring3"""
         result = add_one(5)
         self.assertEqual(result, 6)
 
