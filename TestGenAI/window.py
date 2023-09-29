94e920df44e7539ed290c8459c6bd7fc54a22820
StevePro7 <steveprozune@hotmail.com>
2023-09-29 18:16:14 +0100

    no message

--- TestGenAI/window.py
+++ TestGenAI/window.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-#Define the VAE model
+# Define the VAE model
 latent_dim = 64
 
 # Encoder
@@ -10,6 +10,7 @@ encoder_inputs = tf.keras.Input(shape=(28, 28, 1))
 x = tf.keras.layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
 x = tf.keras.layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
 x = tf.keras.layers.Flatten()(x)
+x = tf.keras.layers.Dense(latent_dim + latent_dim)(x)
 encoder = tf.keras.Model(encoder_inputs, x, name="encoder")
 
 # Decoder
@@ -23,7 +24,21 @@ decoder = tf.keras.Model(latent_inputs, decoder_outputs, name="decoder")
 
 # VAE
 vae_inputs = encoder_inputs
-#vae_outputs = decoder(encoder(encoder_inputs))
 vae_outputs = decoder(encoder(encoder_inputs))
+vae = tf.keras.Model(vae_inputs, vae_outputs, name="vae")
 
-print("9")
+# Load the trained weights (optional)
+vae.load_weights("vae_weights.h5")
+
+# Generate new images
+num_samples = 10
+random_latents = np.random.normal(size=(num_samples, latent_dim))
+generated_images = decoder.predict(random_latents)
+
+# Display the generated images
+fig, axes = plt.subplots(1, num_samples, figsize=(10, 10))
+for i, ax in enumerate(axes):
+    ax.imshow(generated_images[i].reshape(28, 28), cmap="gray")
+    ax.axis("off")
+
+plt.show()
