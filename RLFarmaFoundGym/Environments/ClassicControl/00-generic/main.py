56861bd6940c8409a4674c2c1f9931abd7b2f749
StevePro7 <steveprozune@hotmail.com>
2024-01-12 13:02:02 +0000

    cart pole

---
+++ RLFarmaFoundGym/Environments/ClassicControl/00-generic/main.py
@@ -0,0 +1,15 @@
+import gymnasium as gym
+
+name='Acrobot-v1'
+name='CartPole-v1'
+env = gym.make(name, render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    action = env.action_space.sample()  # agent policy that uses the observation and info
+    observation, reward, terminated, truncated, info = env.step(action)
+
+    if terminated or truncated:
+        observation, info = env.reset()
+
+env.close()
