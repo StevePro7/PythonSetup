16c04fd54641c1e0f114f9efd1fa03265a5ef5c0
StevePro7 <steveprozune@hotmail.com>
2024-01-12 13:06:30 +0000

    box gen

---
+++ RLFarmaFoundGym/Environments/Box2D/00-generic/main.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+
+name="LunarLander-v2"
+
+try:
+    env = gym.make(name, render_mode="human")
+    observation, info = env.reset()
+
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+except KeyboardInterrupt:
+        exit(0)
