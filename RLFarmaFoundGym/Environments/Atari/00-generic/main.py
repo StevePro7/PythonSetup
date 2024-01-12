31beb6b6a3f0c4ce8d3a4308e95fd052d240e789
StevePro7 <steveprozune@hotmail.com>
2024-01-12 13:19:19 +0000

    box3d

--- RLFarmaFoundGym/Environments/Atari/00-generic/main.py
+++ RLFarmaFoundGym/Environments/Atari/00-generic/main.py
@@ -3,14 +3,17 @@ import gymnasium as gym
 #name="ALE/Breakout-v5"
 name="ALE/Krull-v5"
 
-env = gym.make(name, render_mode="human")
-observation, info = env.reset()
+try:
+    env = gym.make(name, render_mode="human")
+    observation, info = env.reset()
 
-for _ in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
 
-    if terminated or truncated:
-        observation, info = env.reset()
+        if terminated or truncated:
+            observation, info = env.reset()
 
-env.close()
+    env.close()
+except KeyboardInterrupt:
+        exit(0)
