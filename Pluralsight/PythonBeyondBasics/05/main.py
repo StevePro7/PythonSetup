e9f64e965488eab5c35469da2e5f7e3dce4d0a79
StevePro7 <steveprozune@hotmail.com>
2024-02-07 18:38:34 +0000

    space

--- Pluralsight/PythonBeyondBasics/05/main.py
+++ Pluralsight/PythonBeyondBasics/05/main.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     sc2 = sc.create_empty("oc2")
     print(sc2.owner_code)
 
-    sc3 = sc.create_with_items("oc3", (1,2,3,4))
+    sc3 = sc.create_with_items("oc3", (1, 2, 3, 4))
     print(sc3.owner_code)
     print(sc3.contents)
