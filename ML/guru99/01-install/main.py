3db2fe95e4f004fd1a439919cf14425caf6df7f2
StevePro7 <steveprozune@hotmail.com>
2024-03-20 20:52:27 +0000

    ML install

---
+++ ML/guru99/01-install/main.py
@@ -0,0 +1,10 @@
+import keras
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+
+if __name__ == '__main__':
+    print(f"keras=='{keras.__version__}'")
+    print(f"numpy=='{np.__version__}'")
+    print(f"tense=='{tf.__version__}'")
+
