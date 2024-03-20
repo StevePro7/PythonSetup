a7d1b01dde181d8bdeee2409a8269bffa60af597
StevePro7 <steveprozune@hotmail.com>
2024-03-20 21:19:37 +0000

    tensor board

--- ML/guru99/01-install/main.py
+++ ML/guru99/01-install/main.py
@@ -2,9 +2,12 @@ import keras
 import numpy as np
 import tensorflow as tf
 import pandas as pd
+import sklearn as sk
 
 if __name__ == '__main__':
     print(f"keras=='{keras.__version__}'")
     print(f"numpy=='{np.__version__}'")
     print(f"tense=='{tf.__version__}'")
+    print(f"panda=='{pd.__version__}'")
+    print(f"scikt=='{sk.__version__}'")
 
