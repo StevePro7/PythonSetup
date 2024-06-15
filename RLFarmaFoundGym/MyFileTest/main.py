6cd67318ac6b5ac8ab5cd8d12df7b6b2bffcdeda
StevePro7 <steveprozune@hotmail.com>
2024-06-15 16:49:03 +0100

    transcript

--- RLFarmaFoundGym/MyFileTest/main.py
+++ RLFarmaFoundGym/MyFileTest/main.py
@@ -1,16 +1,24 @@
 
-def printer() -> str:
-    text: str = None
-    with open('gamefile.txt', 'r') as file:
-        for line in file:
-            text = line.strip()
-            if not text.startswith('#'):
-                break
+def get_content():
+    with open('transcript.txt', 'r') as file:
+        content = file.read()
 
-    return text
+    return content
 
 
 
 if __name__ == '__main__':
-    text: str = printer()
-    print(text)
+    count = 1
+    list = []
+    content = get_content()
+    lines = content.splitlines()
+    for line in lines:
+        if len(line.strip()) == 0:
+            with open('example.txt', 'a') as file:
+                for l in list:
+                    file.write(l)
+                    file.write('\n')
+                list = []
+        else:
+            list.append(line)
+        print(line)
