3d7d3d110d39cbce566d52e5775098c488824c4a
StevePro7 <steveprozune@hotmail.com>
2024-06-15 16:25:04 +0100

    gamefile

--- RLFarmaFoundGym/MyFileTest/main.py
+++ RLFarmaFoundGym/MyFileTest/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def printer() -> str:
+    text: str = None
+    with open('gamefile.txt', 'r') as file:
+        for line in file:
+            text = line.strip()
+            if not text.startswith('#'):
+                break
+
+    return text
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    text: str = printer()
+    print(text)
