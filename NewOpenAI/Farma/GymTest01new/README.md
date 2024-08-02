87433d378d37ac38f964994bdc54d6f3b08ee73d
SteveProXNA <steven_boland@hotmail.com>
2024-08-02 19:57:28 +0100

    gymnasium

---
+++ NewOpenAI/Farma/GymTest01new/README.md
@@ -0,0 +1,13 @@
+Gym Test 01
+02-Aug-2024
+
+Aleksandar Haber	28.17
+Introduction to OpenAI Gym and Frozen Lake Environment in Python- Reinforcement Learning Tutorial
+https://www.youtube.com/watch?v=Vrro7W7iW2w	
+
+pip install gymnasium
+pip install pygame
+pip install --upgrade pip
+
+gymnasium
+0.29.1
---
+++ NewOpenAI/Farma/GymTest01new/main.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+
+name = "FrozenLake-v1"
+try:
+    env = gym.make(name, render_mode="human")
+    observation, info = env.reset()
+    env.render()
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
+except KeyboardInterrupt:
+    exit(0)
