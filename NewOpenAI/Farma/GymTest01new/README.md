03843153ad917a43bf356aeaf01dd9ae15ad6022
SteveProXNA <steven_boland@hotmail.com>
2024-08-03 11:58:31 +0100

    gymtestnew01

--- NewOpenAI/Farma/GymTest01new/README.md
+++ NewOpenAI/Farma/GymTest01new/README.md
@@ -5,13 +5,10 @@ Aleksandar Haber	28.17
 Introduction to OpenAI Gym and Frozen Lake Environment in Python- Reinforcement Learning Tutorial
 https://www.youtube.com/watch?v=Vrro7W7iW2w	
 
+Repeat but use the new FF Gymnasium instead
+pip install -r requirements.txt
 pip install gymnasium
-pip install pygame
-pip install --upgrade pip
 
-gymnasium
-0.29.1
-
-Box2D
-pip install gymnasium[box2d]
-pip install --upgrade pip
+IMPORTANT
+Here is the GitHub repo for this video
+https://github.com/AleksandarHaber/Introduction-to-OpenAI-Gym-Python-Library-and-Frozen-Lake-Reinforcement-Learning-Environment/blob/main/demonstrationFrozenLake.py
--- NewOpenAI/Farma/GymTest01new/main.py
+++ NewOpenAI/Farma/GymTest01new/main.py
@@ -1,21 +1,18 @@
 import gymnasium as gym
 
-#name = "FrozenLake-v1"
-#name = "BipedalWalker-v3"
-#name = "Blackjack-v1"
-#name = "Ant-v4"
-name = "ALE/UpNDown-v5"
+name = "FrozenLake-v1"
 
 try:
     env = gym.make(name, render_mode="human")
     observation, info = env.reset()
-    env.render()
+
     for _ in range(1000):
         action = env.action_space.sample()  # agent policy that uses the observation and info
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
             observation, info = env.reset()
+
     env.close()
 except KeyboardInterrupt:
     exit(0)
