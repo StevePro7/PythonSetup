0198bde698629abe106714a5bc16a1f555d0ec66
SteveProXNA <steven_boland@hotmail.com>
2024-08-03 12:02:04 +0100

    gymtestnew01

--- NewOpenAI/Farma/GymTest01new/main.py
+++ NewOpenAI/Farma/GymTest01new/main.py
@@ -4,14 +4,34 @@ name = "FrozenLake-v1"
 
 try:
     env = gym.make(name, render_mode="human")
-    observation, info = env.reset()
+    env.reset()
+    env.render()
 
-    for _ in range(1000):
-        action = env.action_space.sample()  # agent policy that uses the observation and info
-        observation, reward, terminated, truncated, info = env.step(action)
+    # observation space - states
+    env.observation_space
 
-        if terminated or truncated:
-            observation, info = env.reset()
+    # actions: left -0, down - 1, right - 2, up- 3
+    env.action_space
+
+    randomAction= env.action_space.sample()
+    returnValue = env.step(randomAction)
+    # format of returnValue is (observation,reward, terminated, truncated, info)
+    # observation (object)  - observed state
+    # reward (float)        - reward that is the result of taking the action
+    # terminated (bool)     - is it a terminal state
+    # truncated (bool)      - it is not important in our case
+    # info (dictionary)     - in our case transition probability
+
+    env.render()
+
+    # perform deterministic step 0,1,2,3
+    returnValue = env.step(1)
+    env.reset()
+
+    # env.P[state][action]
+    temp: list = env.P[0][1]
+    # output is a list having the following entries
+    # (transition probability, next state, reward, Is terminal state?)
 
     env.close()
 except KeyboardInterrupt:
