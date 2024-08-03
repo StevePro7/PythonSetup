58f89eec5d659fc50955985fe84cd64d7f5dc7f7
SteveProXNA <steven_boland@hotmail.com>
2024-08-03 11:01:52 +0100

    except

--- RLFarmaFoundGym/Environments/Atari/19-Breakout/main.py
+++ RLFarmaFoundGym/Environments/Atari/19-Breakout/main.py
@@ -1,12 +1,16 @@
 import gymnasium as gym
-env = gym.make("ALE/Breakout-v5", render_mode="human")
-observation, info = env.reset()
 
-for _ in range(1000):
-    action = env.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = env.step(action)
+try:
+    env = gym.make("ALE/Breakout-v5", render_mode="human")
+    observation, info = env.reset()
 
-    if terminated or truncated:
-        observation, info = env.reset()
+    for _ in range(1000):
+        action = env.action_space.sample()  # agent policy that uses the observation and info
+        observation, reward, terminated, truncated, info = env.step(action)
 
-env.close()
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+except KeyboardInterrupt:
+    exit(0)
