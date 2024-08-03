f98a537a424fcf7e5b89438d14397cc621db47f1
StevePro7 <steveprozune@hotmail.com>
2024-08-03 19:23:50 +0100

    additional

---
+++ Medium/FastAPIarticle/routers/api.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+# from routers.v1 import region, city, offer, user, favourite, notification, statistic
+from routers.v1 import region, city
+router = APIRouter(
+    prefix="/api/v1"
+)
+
+# router.include_router(root.router)
+router.include_router(region.router)
+router.include_router(city.router)
+# router.include_router(offer.router)
+# router.include_router(user.router)
+# router.include_router(favourite.router)
+# router.include_router(notification.router)
+# router.include_router(statistic.router)
