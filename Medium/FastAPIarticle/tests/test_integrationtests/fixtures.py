d9a17ae6f833e939ec5366af305d707c456f8432
StevePro7 <steveprozune@hotmail.com>
2024-08-04 17:36:57 +0100

    fixtures

---
+++ Medium/FastAPIarticle/tests/test_integrationtests/fixtures.py
@@ -0,0 +1,34 @@
+import pytest
+# from auth.security import get_password_hash
+# from models.offer import CategoryEnum, SubCategoryEnum, BuildingTypeEnum
+from repository.city_repository import CityRepository
+# from repositories.offer_repository import OfferRepository
+from repository.region_repository import RegionRepository
+# from repositories.user_repository import UserRepository
+from schemas.region_schema import RegionInput
+from schemas.city_schema import CityInput
+# from schemas.offer import OfferScraper
+# from schemas.photo import PhotoInput
+# from schemas.user import UserIn
+# from schemas.favourite import FavouriteInput
+# from repositories.favourite_repository import FavouriteRepository
+# from repositories.notification_repository import NotificationRepository
+# from repositories.notificationfilter_repository import NotificationFilterRepository
+# from schemas.notification import NotificationInput
+# from schemas.notification_filter import NotificationFilterInput
+
+
+@pytest.fixture(scope="function")
+def region(client):
+    test_client, db_session = client
+
+    region = RegionRepository(db_session).create(RegionInput(name="NorthIs"))
+    return region
+
+
+@pytest.fixture(scope="function")
+def city(client, region):
+    test_client, db_session = client
+
+    city = CityRepository(db_session).create(CityInput(name="Auckland", region_id=region.id))
+    return city
