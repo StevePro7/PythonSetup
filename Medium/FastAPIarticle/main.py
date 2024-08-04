0444fdf37dd3e6720d13c130b42d23f5205b0125
StevePro7 <steveprozune@hotmail.com>
2024-08-04 17:55:28 +0100

    main

--- Medium/FastAPIarticle/main.py
+++ Medium/FastAPIarticle/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from routers.api import router
 from utils.init_db import create_tables
+
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -11,13 +14,21 @@ async def lifespan(app: FastAPI):
     print("Application shutdown")
 
 
+origins = ["*"]
 #app = FastAPI(debug=True, title="Tutorial")
 app = FastAPI(debug=True, title="Tutorial", lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World?"}
+
+app.include_router(router)
+
 
 if __name__ == "__main__":
     import uvicorn
