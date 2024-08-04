e6d9745c1f50440efa3d9ea7b4b53e624b32214d
StevePro7 <steveprozune@hotmail.com>
2024-08-04 17:21:58 +0100

    tests

--- Medium/FastAPIarticle/tests/conftest.py
+++ Medium/FastAPIarticle/tests/conftest.py
@@ -11,16 +11,81 @@ from sqlalchemy.orm import sessionmaker
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models import region, city
 from config.database import get_db
-#from routers.api import router
+from routers.api import router
 from utils.init_db import create_tables
 
 engine = create_engine(
     "sqlite:///:memory:",
     connect_args={"check_same_thread": False},
 )
-TestingSessionLocal = sessionmaker(autocommit=True, autoflush=True, bind=engine)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def test_init_db() -> None:
+    #offer.Offer.metadata.create_all(bind=engine)
     region.Region.metadata.create_all(bind=engine)
     city.City.metadata.create_all(bind=engine)
+    #photo.Photo.metadata.create_all(bind=engine)
+    #user.User.metadata.create_all(bind=engine)
+    #favourite.Favorite.metadata.create_all(bind=engine)
+    #notification.Notification.metadata.create_all(bind=engine)
+    #notification_filter.NotificationFilter.metadata.create_all(bind=engine)
+
+
+def start_app():
+    app = FastAPI()
+    app.include_router(router)
+    return app
+
+
+@pytest.fixture()
+def app() -> Generator:
+    create_tables()
+    _app = start_app()
+    yield _app
+
+
+@pytest.fixture
+def test_get_db():
+    test_init_db()
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+        engine.dispose()
+
+
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture(scope="function")
+def db_session(app: FastAPI) -> Generator[SessionTesting, Any, None]:
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = SessionTesting(bind=connection)
+    yield session   # use the session in tests.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def client(
+        app: FastAPI, db_session: SessionTesting
+) -> Generator[TestClient, Any, None]:
+    """
+    Create a new FastAPI TestClient that uses the `db_session` fixture to override
+    the `get_db` dependency that is injected into routes.
+    """
+    test_init_db()
+
+    def _get_test_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = _get_test_db
+    with TestClient(app) as client:
+        yield client, db_session
---
+++ Medium/FastAPIarticle/tests/test_unittests/test_region.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from ..conftest import test_get_db
+from repository.region_repository import RegionRepository
+from schemas.region_schema import RegionInput
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def test_success_create_region_object(test_get_db) -> None:
+    repository = RegionRepository(test_get_db)
+    created_region = repository.create(RegionInput(name="Test Region"))
+    assert created_region.name == "Test Region"
+
+
+def test_success_get_all_regions_objects(test_get_db) -> None:
+    repository = RegionRepository(test_get_db)
+    created_region = repository.create(RegionInput(name="Test Region"))
+    regions = repository.get_all()
+    assert len(regions) == 1
+
+
+def test_success_get_region_by_id(test_get_db) -> None:
+    repository = RegionRepository(test_get_db)
+    created_region = repository.create(RegionInput(name="Test Region"))
+    fetched_region = repository.get_by_id(created_region.id)
+    assert created_region.name == "Test Region"
+
+
+def test_success_update_region(test_get_db) -> None:
+    repository = RegionRepository(test_get_db)
+    created_region = repository.create(RegionInput(name="Test Region"))
+    region = repository.get_by_id(created_region.id)
+    updated_region_input = RegionInput(name="Updated Region")
+    updated_region = repository.update(region, updated_region_input)
+    assert updated_region.name == "Updated Region"
+
+
+def test_success_delete_region(test_get_db) -> None:
+    repository = RegionRepository(test_get_db)
+    created_region = repository.create(RegionInput(name="Test Region"))
+    region = repository.get_by_id(created_region.id)
+    assert repository.delete(region)
