e1ea1bf037c8038e8e451b3c4f749b9bdb289bb7
StevePro7 <steveprozune@hotmail.com>
2024-08-17 21:20:40 +0100

    tests

---
+++ PyBind/README.md
@@ -0,0 +1,5 @@
+# PyBind
+16-Aug-2024
+
+chat.openai.com
+clion python c++ pybind cmakelist example 
---
+++ PyBind/Test01/cmake-build-debug/CMakeFiles/Test01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test01.dir/main.cpp.o"
+  "Test01"
+  "Test01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
