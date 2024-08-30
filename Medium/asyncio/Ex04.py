ffc16a247b039b2384d76f4de7c7cb59e58cd8bc
StevePro7 <steveprozune@hotmail.com>
2024-08-30 21:20:17 +0100

    Exs

--- Medium/asyncio/Ex04.py
+++ Medium/asyncio/Ex04.py
@@ -1,1 +1,13 @@
-# TODO
+import requests
+import time
+
+start_time = time.time()
+
+def fetch(url: str) -> str:
+    return requests.get(url).text
+
+
+page1 = fetch('http://example.com')
+page2 = fetch('http://example.org')
+
+print(f"Done in {time.time() - start_time} seconds")
