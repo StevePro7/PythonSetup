d048e64e27db4791abad0304f08269433539a3f2
StevePro7 <steveprozune@hotmail.com>
2024-10-25 21:39:05 +0100

    Ex01up

--- PyBind/PythonObjectBook/Ex01/foo.c
+++ PyBind/PythonObjectBook/Ex01/foo.c
@@ -3,7 +3,13 @@
 
 int bar()
 {
-    Py_Initialize();
-    Py_Finalize();
+    PyObject * pName = PyUnicode_FromString ( "test_0" );
+    PyObject * pModule = PyImport_Import ( pName );
+    if ( ! pModule )
+    {
+        printf ( "Could not open script.\n" );
+        return 0;
+    }
+
     return 9;
 }
