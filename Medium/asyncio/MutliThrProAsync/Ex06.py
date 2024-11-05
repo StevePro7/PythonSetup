5dae10a06d1eadf80b98d734609d90cc1e7d9926
StevePro7 <steveprozune@hotmail.com>
2024-11-05 17:18:48 +0000

    no message

--- Medium/asyncio/MutliThrProAsync/Ex06.py
+++ Medium/asyncio/MutliThrProAsync/Ex06.py
@@ -1,12 +1,11 @@
-# 06. - Multiprocessing
 import multiprocessing as mp
 
 def process_data(data_chunk):
+    # Perform some data processing here
     return sum(data_chunk)
-
 
 if __name__ == '__main__':
     data = [range(1000000), range(1000000, 2000000), range(2000000, 3000000)]
     with mp.Pool(processes=3) as pool:
         results = pool.map(process_data, data)
-        print() results
+        print(results)
