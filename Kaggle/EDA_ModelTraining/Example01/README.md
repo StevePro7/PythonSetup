7a68ce6b7848cab30d11830bca632e5b93540672
StevePro7 <steveprozune@hotmail.com>
2024-11-09 16:57:36 +0000

    output

--- Kaggle/EDA_ModelTraining/Example01/README.md
+++ Kaggle/EDA_ModelTraining/Example01/README.md
@@ -16,3 +16,24 @@ PACKAGES that could require more attention:
 pandas                  CSV to dataframe
 matplotlib.pyplot       graphs
 seaborn                 correlations
+sklearn                 model selection + training
+
+
+WARNINGS
+sklearn
+ConvergenceWarning: lbfgs failed to converge (status=1):
+STOP: TOTAL NO. of ITERATIONS REACHED LIMIT.
+
+Increase the number of iterations (max_iter) or scale the data as shown in:
+Please also refer to the documentation for alternative solver options:
+
+
+REPORT
+              precision    recall  f1-score   support
+
+           0       0.81      0.80      0.80        99
+           1       0.64      0.65      0.65        55
+
+    accuracy                           0.75       154
+   macro avg       0.72      0.73      0.73       154
+weighted avg       0.75      0.75      0.75       154
---
+++ Kaggle/EDA_ModelTraining/Example01/Test05.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+# Step 1: Loading and Understanding the Data
+data = pd.read_csv('diabetes.csv')
+
+# Step 5: Model Selection and Training with Scikit-Learn
+# splitting the data: divide data into training and test sets
+
+from sklearn.model_selection import train_test_split
+X = data.drop('Outcome', axis=1)
+y = data['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# choose an algortihm
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+# models = {
+#     "Logistic Regression": LogisticRegression(),
+#     "Random Forest": RandomForestClassifier(),
+#     "SVM": SVC()
+# }
+#
+# results = {}
+# for model_name, model in models.items():
+#     model.fit(X_train, y_train)
+#     results[model_name] = model.score(X_test, y_test)
+#
+# print(results)
+
+# evaluating model performance
+# evaluate each model with metrics: accuracy, precision, recall [F1-score]
+from sklearn.metrics import classification_report
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
