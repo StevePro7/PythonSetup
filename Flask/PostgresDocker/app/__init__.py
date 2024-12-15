effcc1bba2461fe243cbb3f7fa02df253b254b55
StevePro7 <steveprozune@hotmail.com>
2024-12-15 13:46:34 +0000

    route

--- Flask/PostgresDocker/app/__init__.py
+++ Flask/PostgresDocker/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.routes import bp
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("app.config.Config")
-    db.init_app(app)
+    app.register_blueprint(bp)
+    #app.config.from_object("app.config.Config")
+    #db.init_app(app)
     return app
