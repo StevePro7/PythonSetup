eb1b836c18b58cf1ade1d621d209d2c1953612e3
StevePro7 <steveprozune@hotmail.com>
2025-06-06 11:37:19 +0100

    reqs

---
+++ Medium/asyncio/CompareClients/README.md
@@ -0,0 +1,4 @@
+Comparing requests, aiohttp, and httpx: Which HTTP client should you use?
+06-Jun-2025
+
+https://leapcell.medium.com/comparing-requests-aiohttp-and-httpx-which-http-client-should-you-use-6e3d9ff47b0e
---
+++ Medium/asyncio/CompareClients/main.py
@@ -0,0 +1,11 @@
+import requests
+import httpx
+import asyncio
+import aiohttp
+import time
+def print_hi(name):
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
