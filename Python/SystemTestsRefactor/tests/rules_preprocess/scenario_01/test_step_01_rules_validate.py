fb05df6477c9b8cf07ed48985eae8128932cd781
StevePro7 <steveprozune@hotmail.com>
2025-06-07 12:22:48 +0100

    no message

--- Python/SystemTestsRefactor/tests/rules_preprocess/scenario_01/test_step_01_rules_validate.py
+++ Python/SystemTestsRefactor/tests/rules_preprocess/scenario_01/test_step_01_rules_validate.py
@@ -1,29 +1,32 @@
 import json
-
+import uuid
 from app.config.constants import LOG_RULES_ENGINE
 from app.rules_process.rules_validate import rules_validate
 from loggers.singleton_logger import SingletonLogger
 
 def test_rules_validate():
 
+
     file_path = 'payloads/test_step_01_inp_obj.json'
     with open(file_path, 'r') as file:
         inp_obj = json.load(file)
 
-
-
-
+    trace_id: str = '0eddf3b3-55e0-4ed7-8798-bdf3baa857a0'
+    process_id: str = "test_01"
 
     log_level: str = "INFO"
     app_env: str = "UAT"
     SingletonLogger.instance(log_level, app_env)
 
-    extra_logger_data = {}
+    extra_logger_data = {
+        'process_id': process_id,
+        'trace_id': trace_id
+    }
 
     context, out_obj = rules_validate(inp_obj, LOG_RULES_ENGINE, extra_logger_data)
-
     assert out_obj is not None
 
+    # Save output
     with open('payloads/test_step_01_context.json', 'w') as file1:
         json.dump(context, file1, indent=2)
 
