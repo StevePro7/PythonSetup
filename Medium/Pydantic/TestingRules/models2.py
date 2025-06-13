0cb9d3c586c8e86c5f23f3769fe65a5282b87afd
StevePro7 <steveprozune@hotmail.com>
2025-06-13 13:58:17 +0100

    models2

---
+++ Medium/Pydantic/TestingRules/models2.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel
+from typing import Optional
+import json
+
+class TestingDataModel(BaseModel):
+    Postgres: Optional[bool] = False
+    Snowflake: Optional[bool] = False
+    RabbitMQ: Optional[bool] = False
+    GCPbucketList: Optional[bool] = False
+    GCPbucketRead: Optional[bool] = False
+    GCPbucketFile: Optional[str] = None
+    LoggerTest: Optional[str] = None
+
+class TestingWrapModel(BaseModel):
+    process_id: Optional[str] = "NULL"
+    testing: Optional[TestingDataModel] = {}
+
+
+testingWrapDict = {
+    "process_id": "adriana",
+    "testing": {
+        "Postgres": False,
+        "Snowflake": False,
+        "RabbitMQ": False,
+        "GCPbucketList": True,
+        "GCPbucketRead": False,
+        "LoggerTest": "Quick brown fox",
+    }
+}
+
+
+testingWrapModel: TestingWrapModel = TestingWrapModel(**testingWrapDict)
+
+
+print(testingWrapModel.testing.GCPbucketList)
+print(testingWrapModel.testing.LoggerTest)
