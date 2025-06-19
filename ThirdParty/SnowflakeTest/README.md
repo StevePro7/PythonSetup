01dc712cde877cfcebc4cbbfbd36b31145538e2c
StevePro7 <steveprozune@hotmail.com>
2025-06-19 17:30:21 +0100

    snowflake

---
+++ ThirdParty/SnowflakeTest/README.md
@@ -0,0 +1,15 @@
+Snowflake Test
+19-Jun-2025
+
+SnowflakeConnection
+
+conn = sfc.connect(
+    user=user,
+    password=password,
+    account=account,
+    role=role,
+    warehouse=warehouse
+)
+
+
+python.exe -m pip install --upgrade pip
---
+++ ThirdParty/SnowflakeTest/main.py
@@ -0,0 +1,9 @@
+import snowflake.connector as sfc
+from snowflake.connector.connection import SnowflakeConnection
+from snowflake.connector.cursor import SnowflakeCursor
+def print_hi(name):
+    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm!!')
