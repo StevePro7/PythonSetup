2b6d865fdc170dd5ae426035bbe1fbb37f2c0b4a
StevePro7 <steveprozune@hotmail.com>
2026-01-28 19:39:17 +0000

    Hyperparam

--- Medium/ML/HyperparameterTuning/main.py
+++ Medium/ML/HyperparameterTuning/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from hyperparametertuning!")
+# Import necessary libraries
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
+# Import necessary libraries (if not already imported)
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint, uniform
+from sklearn.metrics import accuracy_score, classification_report
 
-if __name__ == "__main__":
-    main()
+print('the end2')
