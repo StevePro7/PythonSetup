6d7f55b42d0c6029560fec875e2b09028a6a0645
StevePro7 <steveprozune@hotmail.com>
2026-02-16 16:17:05 +0000

    Pt02b

--- PyBind/new-02/01-stevechange-03/cpp/src/api/my_api.h
+++ PyBind/new-02/01-stevechange-03/cpp/src/api/my_api.h
@@ -7,14 +7,22 @@ template <typename T>
 class Container
 {
 public:
-    Container(const size_t num_elements);
+    Container(const size_t num_elements)
+    {
+        mData = std::make_unique<T[]>(num_elements);
+    }
 
-    T& operator [](const size_t index);
+    T& operator [](const size_t index)
+    {
+        return mData[index];
+    }
 
-    const T& operator [](const size_t index) const;
+    const T& operator [](const size_t index) const
+    {
+        return mData[index];
+    }
 
 private:
     std::unique_ptr<T[]> mData;
 };
-
 #endif//_MY_API_H_
--- PyBind/new-02/01-stevechange-03/python/main.py
+++ PyBind/new-02/01-stevechange-03/python/main.py
@@ -1,5 +1,15 @@
-import my_api_py
+from my_api_py import Container
+
 print("beg")
+
+container = Container(10)
+
+for i in range(0, 10):
+    container[i] = i
+
+
+for i in range(0, 10):
+    print(container[i])
 
 
 print("end")
