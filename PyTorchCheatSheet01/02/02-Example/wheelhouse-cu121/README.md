90ddcefece5d8f2ad72bf491fc6a6a1789fe47a9
StevePro7 <steveprozune@hotmail.com>
2026-04-06 20:30:49 +0100

    dedup

--- PyTorchCheatSheet01/02/02-Example/wheelhouse-cu121/README.md
+++
@@ -1,3 +0,0 @@
-Wheelhouse files live here!
-stevepropytorch3d-0.7.7-cp310-cp310-linux_x86_64.whl
-steveprotorchsparse-2.0.0b0-cp310-cp310-linux_x86_64.whl
--- PyTorchCheatSheet01/02/05-Example/app/utils/device.py
+++
@@ -1,8 +0,0 @@
-# app/utils/device.py
-import torch
-import torch._C
-
-
-def get_device():
-    cuda: torch._C.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    return cuda
