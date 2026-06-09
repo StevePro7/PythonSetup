ac4e0f8eaa38120c6a216f3d4a63307013dba11a
StevePro7 <steveprozune@hotmail.com>
2026-06-09 22:59:53 +0100

    splash

--- PyGame/Testing/Screens/SplashScreen.py
+++ PyGame/Testing/Screens/SplashScreen.py
@@ -1,4 +1,6 @@
 import pygame
+
+import enumerations
 from MyGame import MyGame
 from Screens.BaseScreen import BaseScreen
 from Static.Assets import Assets
@@ -9,6 +11,7 @@ from enumerations import ScreenType
 class SplashScreen(BaseScreen):
 
     def __init__(self):
+        self.splashDelay: int = None
         self.position: pygame.Vector2 = None
 
 
@@ -17,13 +20,22 @@ class SplashScreen(BaseScreen):
 
 
     def LoadContent(self) -> None:
+        super().LoadContent()
+        self.splashDelay = MyGame.Manager.ConfigManager.ConfigData.SplashDelay
         wide: int = (const.SCREEN_WIDE - Assets.SplashTexture.get_width()) / 2
         high: int = (const.SCREEN_HIGH - Assets.SplashTexture.get_height()) / 2
         self.position: pygame.Vector2 = pygame.Vector2(wide, high)
 
 
     def Update(self, deltaTime: int) -> ScreenType | None:
-        #return ScreenType.Init
+        super().UpdateTimer(deltaTime)
+        if self.Timer > self.splashDelay:
+            return enumerations.ScreenType.Title
+
+        fullScreen: bool = MyGame.Manager.InputManager.FullScreen()
+        if fullScreen:
+            return enumerations.ScreenType.Title
+
         return None
 
 
