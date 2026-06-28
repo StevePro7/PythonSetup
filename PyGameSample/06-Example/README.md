b0094656a5cf23feacbe3aa1f46abf9962e3a960
StevePro7 <steveprozune@hotmail.com>
2026-06-28 13:59:07 +0100

    wip

---
+++ PyGameSample/06-Example/README.md
@@ -0,0 +1,40 @@
+## Example VI
+#### 15-Sep-2026
+
+### Hello Sound
+Launch PyCharm | New Project
+
+| KEY | VALUE           |
+| :---   |:----------------|
+| Location: | ~/HelloSound    |
+| Interpreter type: | uv              |
+| Python version: | 3.11            |
+| Path to uv: | ~/.local/bin/uv |
+
+Create
+
+Setup environment - if not auto created by PyCharm
+```sh
+uv venv --python 3.11
+source .venv/bin/activate       # OR .\.venv\Scripts\activate
+which python
+`which python` --version	# Python 3.11.11
+```
+
+UV update pyproject.toml
+```sh
+uv lock
+uv sync
+```
+
+Write code for main program
+```sh
+main.py
+```
+
+Finally hit F5 to run Python code
+```sh
+uv run main.py
+```
+
+OUTPUT
---
+++ PyGameSample/07-Example/main.py
@@ -0,0 +1,31 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Hello Music")
+
+font = pygame.font.Font(None, 48)  # Built-in default font
+
+text1 = font.render("Hello Music!", True, (255, 255, 255))
+text2 = font.render("Press Up - play music", True, (255, 255, 255))
+text3 = font.render("Press Down - stop music", True, (255, 255, 255))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    screen.blit(text1, (220, 220))
+    screen.blit(text2, (160, 260))
+    screen.blit(text3, (160, 300))
+
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    #     space = font.render("Space pressed!!", True, (255, 255, 255))
+    #     screen.blit(space, (200, 260))
+
+    pygame.display.flip()
+
+pygame.quit()
